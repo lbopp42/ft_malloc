adc20e15771eacd93a773bb79570fcec42baca96
Lucas BOPP <lbopp@student.42.fr>
2019-01-17 13:17:47 +0100

    calloc.c norme OK

--- src/calloc.c
+++ src/calloc.c
@@ -6,7 +6,7 @@
 /*   By: lbopp <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/17 10:23:28 by lbopp             #+#    #+#             */
-/*   Updated: 2019/01/17 10:23:30 by lbopp            ###   ########.fr       */
+/*   Updated: 2019/01/17 13:17:19 by lbopp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	*calloc(size_t count, size_t size)
 {
-    void	*new;
+	void	*new;
 
-    if (!(new = malloc(count * size)))
-	return (NULL);
-    ft_bzero(new, count * size);
-    return (new);
+	if (!(new = malloc(count * size)))
+		return (NULL);
+	ft_bzero(new, count * size);
+	return (new);
 }
