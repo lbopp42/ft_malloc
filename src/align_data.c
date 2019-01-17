1c718e7b902cd57dbc4b184b29b1cc99c9b4771a
Lucas BOPP <lbopp@student.42.fr>
2019-01-17 13:19:44 +0100

    align_data.c norme ok

--- src/align_data.c
+++ src/align_data.c
@@ -6,7 +6,7 @@
 /*   By: lbopp <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/17 12:55:22 by lbopp             #+#    #+#             */
-/*   Updated: 2019/01/17 12:56:38 by lbopp            ###   ########.fr       */
+/*   Updated: 2019/01/17 13:19:23 by lbopp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,23 @@
 
 size_t	align_size(size_t init_size)
 {
-    if (init_size < ALIGN)
-	return (ALIGN);
-    else if (init_size % ALIGN == 0)
-	return (init_size);
-    else
-	return (init_size + ALIGN - init_size % ALIGN);
+	if (init_size < ALIGN)
+		return (ALIGN);
+	else if (init_size % ALIGN == 0)
+		return (init_size);
+	else
+		return (init_size + ALIGN - init_size % ALIGN);
 }
 
 size_t	round_page_size(size_t init_size)
 {
-    size_t	os_page_size;
+	size_t	os_page_size;
 
-    os_page_size = (size_t)getpagesize();
-    if (init_size < os_page_size)
-	return (os_page_size);
-    else if (init_size % os_page_size == 0)
-	return (init_size);
-    else
-	return (init_size + os_page_size - init_size % os_page_size);
+	os_page_size = (size_t)getpagesize();
+	if (init_size < os_page_size)
+		return (os_page_size);
+	else if (init_size % os_page_size == 0)
+		return (init_size);
+	else
+		return (init_size + os_page_size - init_size % os_page_size);
 }
