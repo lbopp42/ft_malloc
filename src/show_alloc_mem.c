bc2d1ce4723650dcfcc330826d98e5da50e7e3ba
Lucas BOPP <lbopp@student.42.fr>
2019-01-17 15:35:27 +0100

    show_alloc_mem.c norme ok

--- src/show_alloc_mem.c
+++ src/show_alloc_mem.c
@@ -6,7 +6,7 @@
 /*   By: lbopp <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/17 10:47:29 by lbopp             #+#    #+#             */
-/*   Updated: 2019/01/17 11:15:23 by lbopp            ###   ########.fr       */
+/*   Updated: 2019/01/17 14:57:35 by lbopp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,66 +14,75 @@
 
 char	hex_digit(int v)
 {
-    if (v >= 0 && v < 10)
-	return '0' + v;
-    else
-	return 'a' + v - 10;
+	if (v >= 0 && v < 10)
+		return ('0' + v);
+	else
+		return ('a' + v - 10);
 }
 
 void	print_mem(void *p0)
 {
-    int i;
-    uintptr_t p;
+	int			i;
+	uintptr_t	p;
 
-    p = (uintptr_t)p0;
-    i = (sizeof(p) << 3) - 4;
-    while (i >= 0)
-    {
-	ft_putchar(hex_digit((p >> i) & 0xf));
-	i -= 4;
-    }
+	p = (uintptr_t)p0;
+	i = (sizeof(p) << 3) - 4;
+	while (i >= 0)
+	{
+		ft_putchar(hex_digit((p >> i) & 0xf));
+		i -= 4;
+	}
+}
+
+void	print_each_data(const t_page *page, size_t *total)
+{
+	t_meta	*data;
+
+	data = (t_meta*)((char*)page + align_size(sizeof(t_page*)));
+	while (data && !data->is_free)
+	{
+		print_mem((char*)data + sizeof(t_meta));
+		ft_putstr(" - ");
+		print_mem((char*)data + sizeof(t_meta) + data->size);
+		ft_putstr(" : ");
+		ft_putnbr(data->size);
+		ft_putendl(" octets");
+		*total += data->size;
+		data = data->next;
+	}
+}
+
+void	print_type(int type)
+{
+	if (type == 0)
+		ft_putstr("TINY : ");
+	else if (type == 1)
+		ft_putstr("SMALL : ");
+	else
+		ft_putstr("LARGE : ");
 }
 
 void	show_alloc_mem(void)
 {
-    t_page	*page;
-    t_meta	*origin;
-    size_t	total;
-    int		i;
+	t_page	*page;
+	size_t	total;
+	int		type;
 
-    total = 0;
-    i = 0;
-    while (i < 3)
-    {
-	page = g_zone[i].page;	
-	while (page)
+	total = 0;
+	type = -1;
+	while (++type < 3)
 	{
-	    if (i == 0)
-		ft_putstr("TINY : ");
-	    else if (i == 1)
-		ft_putstr("SMALL : ");
-	    else
-		ft_putstr("LARGE : ");
-	    print_mem(page);
-	    ft_putchar('\n');
-	    origin = (t_meta*)((char*)page + align_size(sizeof(t_page*)));
-	    while (origin && !origin->is_free)
-	    {
-		print_mem((char*)origin + sizeof(t_meta));
-		ft_putstr(" - ");
-		print_mem((char*)origin + sizeof(t_meta) + origin->size);
-		ft_putstr(" : ");
-		ft_putnbr(origin->size);
-		ft_putendl(" octets");
-		total += origin->size;
-		origin = origin->next;
-	    }
-	    page = page->next;
+		page = g_zone[type].page;
+		while (page)
+		{
+			print_type(type);
+			print_mem(page);
+			ft_putchar('\n');
+			print_each_data(page, &total);
+			page = page->next;
+		}
 	}
-	i++;
-    }
-    ft_putstr("Total : ");
-    ft_putnbr(total);
-    ft_putendl(" octects");
+	ft_putstr("Total : ");
+	ft_putnbr(total);
+	ft_putendl(" octects");
 }
-
