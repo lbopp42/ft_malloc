0bc532ad8440455439cb76232e3b8dc933141407
Lucas BOPP <lbopp@student.42.fr>
2019-01-17 13:16:09 +0100

    realloc.c norme OK

--- src/realloc.c
+++ src/realloc.c
@@ -6,7 +6,7 @@
 /*   By: lbopp <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/17 10:28:17 by lbopp             #+#    #+#             */
-/*   Updated: 2019/01/17 10:40:43 by lbopp            ###   ########.fr       */
+/*   Updated: 2019/01/17 13:15:43 by lbopp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,36 @@
 
 void	free_without_check(void *ptr, t_zone_id ret)
 {
-    t_meta		*data;
+	t_meta	*data;
 
-    data = (t_meta*)((char*)ptr - sizeof(t_meta));
-    data->is_free = 1;
-    free_defrag(&data);
-    try_remove_page(ret, data);
+	data = (t_meta*)((char*)ptr - sizeof(t_meta));
+	data->is_free = 1;
+	free_defrag(&data);
+	try_remove_page(ret, data);
 }
 
 void	*realloc(void *ptr, size_t size)
 {
-    char	*new;
-    t_meta	*data;
-    t_zone_id	ret;
+	char		*new;
+	t_meta		*data;
+	t_zone_id	ret;
 
-    if (!ptr)
-	return (malloc(size));
-    if (size == 0 && ptr)
-    {
-	free(ptr);
-	return (malloc(ALIGN));
-    }
-    if (!(ret = find_zone_free(ptr)).page)
-	return (NULL);
-    data = (t_meta*)((char*)ptr - sizeof(t_meta));
-    if (!check_data(ret.page, data))
-	return (NULL);
-    if (data->size >= align_size(size))
-	return (ptr);
-    new = malloc(size);
-    ft_memcpy(new, ptr, data->size);
-    free_without_check(ptr, ret);
-    return (new);
+	if (!ptr)
+		return (malloc(size));
+	if (size == 0 && ptr)
+	{
+		free(ptr);
+		return (malloc(ALIGN));
+	}
+	if (!(ret = find_zone_free(ptr)).page)
+		return (NULL);
+	data = (t_meta*)((char*)ptr - sizeof(t_meta));
+	if (!check_data(ret.page, data))
+		return (NULL);
+	if (data->size >= align_size(size))
+		return (ptr);
+	new = malloc(size);
+	ft_memcpy(new, ptr, data->size);
+	free_without_check(ptr, ret);
+	return (new);
 }
